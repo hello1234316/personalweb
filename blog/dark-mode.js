01e32f6fee8f6930c278c5035b449b931c6a1427
HELLO Phone <85028651+hello1234316@users.noreply.github.com>
2021-12-23 12:31:26 +0800

    Create dark-mode.js

---
+++ blog/dark-mode.js
@@ -0,0 +1,22 @@
+if (window.matchMedia('(prefers-color-scheme)').media === 'not all') {
+    // console.log('Themes not supported, falling back to light theme.');
+    document.documentElement.style.display = 'none';
+    document.head.insertAdjacentHTML(
+        'beforeend',
+        '<link rel="stylesheet" href="styles/index-light.css" onload="document.documentElement.style.display = ``">'
+    );
+}
+
+var themeColor = null;
+if (window.matchMedia('(prefers-color-scheme: dark)').matches == true) {
+    // console.log('Dark mode is enabled.');
+    themeColor = '#1565c0';
+} else {
+    // console.log('Dark mode is disabled.');
+    themeColor = '#039be5';
+}
+
+document.head.insertAdjacentHTML(
+    'beforeend',
+    '<meta content="' + themeColor + '" name="theme-color">'
+);
